9d859b71dfb8ee57782336c27d9677f77c41e63c
Noah <noahyasskin@gmail.com>
2016-09-09 15:43:19 -0700

    add route removal and root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root :to => 'topics#index'
+
   resources :topics do
-    resources :ailments
+    resources :ailments, :except => [:show, :index]
   end
-  
+
 end
