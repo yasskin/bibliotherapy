08720bca10aa22bbb0fcbc7cb56b5492c2744a20
Noah <noahyasskin@gmail.com>
2016-09-16 09:56:37 -0700

    add Devise authentication gem and Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@
 topic = Topic.create(name: 'Boredom')
 
 topic.ailment << Topic.find_by_name("Look inward")
+
+User.create!(email: "me@home.com", password: "watching the telly")
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
+  factory :user do
+    
+  end
   factory(:topic) do
     name('Love')
   end
