5bbe78f59aefc45c9fdfab0d12500cda883f3d8e
Noah <noahyasskin@gmail.com>
2016-09-23 15:10:46 -0700

    add AJAX JS forms for New and Edit Views for Cure Model

--- app/controllers/cures_controller.rb
+++ app/controllers/cures_controller.rb
@@ -3,6 +3,10 @@
    def new
      @ailment = Ailment.find(params[:ailment_id])
      @cure = @ailment.cures.new
+     respond_to do |format|
+       format.html { render :new }
+       format.js
+     end
    end
 
    def create
@@ -14,6 +18,33 @@
      else
        render :new
      end
+   end
+
+   def edit
+     @ailment = Ailment.find(params[:ailment_id])
+     @cure = Cure.find(params[:id])
+     respond_to do |format|
+       format.html { render :edit }
+       format.js
+     end
+   end
+
+   def update
+     @ailment = Ailment.find(params[:ailment_id])
+     @cure = Cure.find(params[:id])
+     if @cure.update(cure_params)
+       flash[:notice] = "Cure successfully updated!"
+       redirect_to ailment_path(@cure.ailment)
+     else
+       render :edit
+     end
+   end
+
+   def destroy
+     @cure = Cure.find(params[:id])
+     @cure.destroy
+     flash[:notice] = "Cure successfully deleted"
+     redirect_to ailment_path(@cure.ailment)
    end
 
 private
