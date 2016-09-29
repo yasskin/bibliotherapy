687d0dbed519797ab6475049a0a94ba6773f2fc7
Noah <noahyasskin@gmail.com>
2016-09-28 17:45:07 -0700

    add JS to sign up error add user pages spec

--- spec/features/add_user_pages_spec.rb
+++ spec/features/add_user_pages_spec.rb
@@ -10,9 +10,10 @@ describe "the add a user process" do
     expect(page).to have_content 'Welcome'
   end
 
-  it "gives error when no title is entered" do
+  it "gives error when no user is entered", js: true do
     visit new_user_registration_path
     click_on 'Sign up'
+    click_button "Sign up"
     expect(page).to have_content 'errors'
   end
 end
@@ -20,14 +21,10 @@ end
 describe "user log in" do
   it "allows an existing user to sign in", js: true do
     user = FactoryGirl.create(:user)
-
     visit new_user_session_path
-
     fill_in "Email", with: "test@email.com"
     fill_in "Password", with: "password"
-
     click_button "Log in"
-
     expect(page).to have_content("Signed in successfully.")
   end
 end
