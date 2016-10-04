09e865bd57f7ad582fd3e25964a2b6d54d254d81
Noah <noahyasskin@gmail.com>
2016-10-04 11:54:00 -0700

    simplified Add Cure Spec

--- spec/features/add_cure_pages_spec.rb
+++ spec/features/add_cure_pages_spec.rb
@@ -12,7 +12,7 @@ describe "the add a cure process" do
     fill_in 'Therapy', :with => 'Regain reason'
     fill_in 'Author', :with => 'Plato'
     fill_in 'Book title', :with => 'Symposium'
-    fill_in 'Book cover', :with => 'https://www.amazon.com/Birth-Love-Novel-Joanna-Kavenna-ebook/dp/B003H3IOL4/ref=as_li_ss_il?s=books&ie=UTF8&qid=1475605857&sr=1-1&keywords=the+birth+of+love+kavenna&linkCode=li2&tag=timeoutbooks-20&linkId=462eedbcb8cadbcfa62016d5ab4fc81e'
+    fill_in 'Book cover', :with => 'https://www.amazon.com/'
     click_button 'Create Cure'
     expect(page).to have_content 'Cure successfully created!'
   end
@@ -28,7 +28,7 @@ describe "the add a cure process" do
     fill_in 'Therapy', :with => 'Regain reason'
     fill_in 'Author', :with => 'Plato'
     fill_in 'Book title', :with => 'Symposium'
-    fill_in 'Book cover', :with => 'https://www.amazon.com/Birth-Love-Novel-Joanna-Kavenna-ebook/dp/B003H3IOL4/ref=as_li_ss_il?s=books&ie=UTF8&qid=1475605857&sr=1-1&keywords=the+birth+of+love+kavenna&linkCode=li2&tag=timeoutbooks-20&linkId=462eedbcb8cadbcfa62016d5ab4fc81e'
+    fill_in 'Book cover', :with => 'https://www.amazon.com/'
     click_button 'Create Cure'
     expect(page).to have_content 'errors'
   end
